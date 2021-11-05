2bdc8169a5fa474ce29c8f5d81f0cc905927c683
Bhushan <bhushansawant48@gmail.com>
2021-11-06 04:26:34 +0530

    fist commit

---
+++ src/test/java/qaclickacademy/MavenJava/FacebookTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class FacebookTest 
+{
+
+	
+        @Test
+       public void fblogin()
+       {
+    	   System.out.println("facebook login");
+       }
+
+}
+
+
+		
+
+
+
---
+++ src/test/java/qaclickacademy/MavenJava/GmailTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class GmailTest
+
+{
+  @Test
+  public void gmaillogin()
+  {
+	  System.out.println("Gmail Login");
+  }
+}
